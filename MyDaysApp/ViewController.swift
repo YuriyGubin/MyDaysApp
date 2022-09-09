5fb947e33ecf1e1746af1ce8c4a81030b086f2c4
YuriyGubin <iosdevelop.2022@gmail.com>
2022-09-09 22:55:35 +0500

    Add code. App is Ready

--- MyDaysApp/ViewController.swift
+++ MyDaysApp/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var resultButton: UIButton!
+    
+    private var numberOfDays = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        resultButton.layer.cornerRadius = 15
+        
     }
 
-
+    @IBAction func resultButtonTapped() {
+        infoLabel.text = "You enjoy your life for \(numberOfDays) days"
+    }
+    
+    @IBAction func datePicker(_ sender: UIDatePicker) {
+        let range = sender.date..<Date.now
+        numberOfDays = range.formatted(.components(style: .wide, fields: [.day]))
+    }
 }
 
